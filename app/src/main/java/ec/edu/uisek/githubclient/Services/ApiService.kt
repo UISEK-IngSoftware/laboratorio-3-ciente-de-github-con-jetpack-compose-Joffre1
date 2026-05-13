40782d17b58508d7c1f56eb33fcd79366541d82f
LABORATORIO FELIPE SEGOVIA <laboratorio.fs@ad.uisek.edu.ec>
2026-05-13 11:23:48 -0500

    Laboratorio 5 terminado

--- app/src/main/java/ec/edu/uisek/githubclient/Services/ApiService.kt
+++ app/src/main/java/ec/edu/uisek/githubclient/Services/ApiService.kt
@@ -1,7 +1,10 @@
 package ec.edu.uisek.githubclient.services
 import androidx.compose.ui.text.style.TextDirection
 import ec.edu.uisek.githubclient.models.Repository
+import ec.edu.uisek.githubclient.models.RepositoryPayload
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface ApiService {
@@ -9,7 +12,12 @@ interface ApiService {
     suspend fun getRepository (
         @Query("sort") sort: String = "Created",
         @Query("direction") direction: String = "desc",
-        @Query("affiliation") afiliation: String = "owner",
+        @Query("affiliation") affiliation: String = "owner",
         @Query("t") t: String = "${System.currentTimeMillis()}"
     ): List<Repository>
+
+    @POST("user/repos")
+    suspend fun createRepository (
+        @Body payload: RepositoryPayload
+    ): Repository
 }
--- app/src/main/java/ec/edu/uisek/githubclient/ui/screens/RepoList.kt
+++ app/src/main/java/ec/edu/uisek/githubclient/ui/screens/RepoList.kt
@@ -5,8 +5,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -23,40 +30,66 @@ import ec.edu.uisek.githubclient.ui.viewModels.RepoListViewModel
 @Composable
 fun RepoList(
     modifier: Modifier = Modifier,
-    viewModel: RepoListViewModel = viewModel()
+    viewModel: RepoListViewModel = viewModel(),
+    onNavigateToForm: () -> Unit = {}
 ){
     val repos by viewModel.repos.collectAsState()
     val isLoading by viewModel.isLoading.collectAsState()
     val errorMsg by viewModel.errorMsg.collectAsState()
 
-    Box (
-        modifier = Modifier
-            .fillMaxSize()
-            .padding()
-    ){
-        if (isLoading){
-            CircularProgressIndicator(
-                modifier = Modifier.align(Alignment.Center)
-            )
+    Scaffold (
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onNavigateToForm,
+                shape = CircleShape,
+                elevation = FloatingActionButtonDefaults.elevation(8.dp),
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Añadir Repositorio"
+                )
+            }
         }
-        errorMsg?.let { message ->
-            Text(
-                text = message,
-                color = MaterialTheme.colorScheme.error,
-                modifier = Modifier.align(Alignment.Center)
-                    .padding(16.dp)
-            )
-        }
-        if (!isLoading && errorMsg ==null){
-            LazyColumn (
-                modifier = Modifier
-                    .fillMaxSize()
-            ){
-                items(repos.size){i ->
-                    RepoItem(repos[i])
+    ) { paddingValues ->
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            if (isLoading) {
+                CircularProgressIndicator(
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+            errorMsg?.let { message ->
+                Text(
+                    text = message,
+                    color = MaterialTheme.colorScheme.error,
+                    modifier = Modifier.align(Alignment.Center)
+                        .padding(16.dp)
+                )
+            }
+            if (!isLoading && errorMsg == null) {
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    items(repos.size) { i ->
+                        RepoItem(repos[i])
+                    }
                 }
             }
         }
     }
 }
 
+@Preview
+@Composable
+fun RepoListPreview(){
+    RepoList()
+
+}
